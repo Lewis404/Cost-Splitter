745e977603364e34f46fe8643194bd928753c14b
Lewis404 <birkslewis@gmail.com>
2017-06-27 17:42:02 +0100

    Removed Lombok dependency

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,9 @@
-import lombok.*;
-
 /**
  * Created by Lewis on 10/11/2016.
  */
 public class Person {
 
-    @Getter
-    @Setter
     private String name;
-
-    @Getter
-    @Setter
     private double totalCost;
 
     public Person(String name, double cost) {
@@ -18,21 +11,21 @@ public class Person {
         this.setTotalCost(cost);
     }
 
-//    public double getTotalCost() {
-//        return totalCost;
-//    }
-//
-//    public void setTotalCost(double totalCost) {
-//        this.totalCost = totalCost;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void addCost(double cost){
         this.totalCost += cost;
